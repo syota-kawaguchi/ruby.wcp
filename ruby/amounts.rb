3f31d52b2956642e97e1b26b45d466455f91166d
EC2 Default User <ec2-user@ip-172-31-3-101.ec2.internal>
2021-02-13 11:53:24 +0000

    [Finish] 1 - 7

---
+++ ruby/amounts.rb
@@ -0,0 +1,8 @@
+amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
+amounts.each do |fruit, amount|
+  puts "#{fruit}は#{amount}です。"
+end
+
+(1..6).each do |num|
+  puts num
+end
---
+++ ruby/lesson7-1.rb
@@ -0,0 +1,8 @@
+puts "計算を始めます。"
+puts "2つの値を入力してください"
+a = gets
+b = gets
+puts "計算結果を出力します。"
+answer = a.to_i * b.to_i
+puts "a*b = #{answer}"
+puts "計算を終了します。"
